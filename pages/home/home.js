d538f738be35125eb0d414c2558ab10916e8d3c4
helloworld1213 <237786429@qq.com>
2020-07-17 11:08:24 +0800

    知识点1

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,13 +1,38 @@
 // pages/home/home.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    name: 'coder',
+    age: 22,
+    movies: [
+      {id: 1, name: '飞鹰计划'},
+      {id: 2, name: '龙的心'},
+      {id: 3, name: '新警察故事1'},
+      {id: 4, name: '醉拳'}
+    ],
+    counter: 0
+    
   },
+  add() {
+    //1.错误的做法:数据发生了变化,但是界面不会刷新
+    //console.log(this);
+    // console.log(this.data.counter);
+    //注意,这里的this指的不是data,
+    // this.data.counter++;
 
+    //2.this.setData()
+    this.setData({
+      //这里不能使用counter++,不起作用
+      counter: this.data.counter + 1,
+    })
+  },
+  reduce() {
+    this.setData({
+      counter: this.data.counter - 1,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
